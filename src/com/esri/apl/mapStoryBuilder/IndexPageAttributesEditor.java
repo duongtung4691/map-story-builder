6e7307477319ee5d9663516c47a95b7fc7b15a84
Mark Deaton <mdeaton@esri.com>
2014-01-09 15:08:26 -0800

    201312

--- src/com/esri/apl/mapStoryBuilder/IndexPageAttributesEditor.java
+++ src/com/esri/apl/mapStoryBuilder/IndexPageAttributesEditor.java
@@ -7,6 +7,7 @@ import pl.polidea.coverflow.AbstractCoverFlowImageAdapter;
 import pl.polidea.coverflow.CoverFlow;
 import android.app.Activity;
 import android.content.Context;
+import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.os.AsyncTask.Status;
@@ -25,6 +26,7 @@ import com.dropbox.client2.session.AccessTokenPair;
 import com.dropbox.client2.session.AppKeyPair;
 import com.dropbox.client2.session.Session.AccessType;
 import com.esri.apl.mapStoryBuilder.tasks.UploadMapStoryToDropbox;
+import com.esri.apl.mapStoryBuilder.tasks.UploadMapStoryToDropboxSvc;
 import com.esri.apl.mapStoryBuilder.tasks.ZipAndShip;
 import com.esri.apl.mapStoryBuilder.utils.FileUtils;
 import com.esri.apl.mapStoryBuilder.utils.PrefsUtils;
@@ -38,9 +40,9 @@ public class IndexPageAttributesEditor extends Activity implements OnClickListen
 	private File							localStoryDir;
 	private Button							btnUpload;
 	private Button							btnSendArchive;
-	private UploadMapStoryToDropbox 		uploadTask;
+//	private UploadMapStoryToDropbox 		uploadTask;
 	private ZipAndShip						zipShipTask;
-	private DropboxAPI<AndroidAuthSession>	mDBApi;
+//	private DropboxAPI<AndroidAuthSession>	mDBApi;
 	
 	private EditText						txtMapTitle;
 	private EditText						txtMapSubtitle;
@@ -56,14 +58,14 @@ public class IndexPageAttributesEditor extends Activity implements OnClickListen
 		super.onCreate(savedInstanceState);
 
 		// Retoring from configuration change? Then restore tasks, too
-		UploadMapStoryToDropbox task = (UploadMapStoryToDropbox)getLastNonConfigurationInstance();
-		if (task != null && task.getStatus() != Status.FINISHED) {
+//		UploadMapStoryToDropbox task = (UploadMapStoryToDropbox)getLastNonConfigurationInstance();
+/*		if (task != null && task.getStatus() != Status.FINISHED) {
 			uploadTask = task;
 			uploadTask.attach(IndexPageAttributesEditor.this);
-		}
+		}*/
 		// Other OnCreate setup
-		String sStoryDir = getIntent().getStringExtra(getString(R.string.chosenStoryName));
-		localStoryDir = new File(getExternalFilesDir(null), sStoryDir);
+		String sMapStoryName = getIntent().getStringExtra(getString(R.string.chosenStoryName));
+		localStoryDir = new File(getExternalFilesDir(null), sMapStoryName);
 		setContentView(R.layout.editindexpageattributes);
 		cfPhotoGallery = (CoverFlow)findViewById(R.id.cfPhotoGallery);
 		PhotoDirCoverFlowAdapter adapter = new PhotoDirCoverFlowAdapter(this);
@@ -76,6 +78,8 @@ public class IndexPageAttributesEditor extends Activity implements OnClickListen
 		btnSendArchive.setOnClickListener(this);
 		
 		txtMapTitle = (EditText)findViewById(R.id.txtMapTitle);
+		txtMapTitle.setText(sMapStoryName);
+		
 		txtMapSubtitle = (EditText)findViewById(R.id.txtMapSubtitle);
 		txtIntroTitle = (EditText)findViewById(R.id.txtIntroTitle);
 		txtIntroDesc = (EditText)findViewById(R.id.txtIntroDesc);
@@ -133,6 +137,43 @@ public class IndexPageAttributesEditor extends Activity implements OnClickListen
 			String secret = PrefsUtils.getPref(context, context.getString(R.string.dropboxUserToken_secret));
 			AndroidAuthSession session = buildDropboxSession(key, secret);
 			
+			DropboxAPI<AndroidAuthSession> dbApi = new DropboxAPI<AndroidAuthSession>(session);
+			((MapStoryBuilderApplication) getApplication()).dbApi = dbApi; 
+			
+			// 3 possibilities:			
+			if (!dbApi.getSession().isLinked()) {
+				// Possibility 1: User not yet signed in; need to initiate authentication
+				// (this will invoke another application, then return; upload is handled in onResume)
+				currentlyAuthenticating = true;
+				dbApi.getSession().startAuthentication(context);
+			}
+			else {
+				// Possibility 2: User already signed in; continue with upload
+				/*uploadTask = new UploadMapStoryToDropbox(localStoryDir.getName(), chkUploadWebTemplateFiles.isChecked(), IndexPageAttributesEditor.this);
+				uploadTask.execute(mDBApi);*/
+				uploadFiles();
+			}
+			// Possibility 3: User tried to sign in but failed for some reason
+			// (this is handled in onResume)
+		}
+		else if (v == btnSendArchive) {
+			zipShipTask = new ZipAndShip(localStoryDir.getName(), chkUploadWebTemplateFiles.isChecked(), this);
+			zipShipTask.execute();
+		}
+	}
+/*	@Override
+	public void onClick(View v) {
+		// First update index.html config section with user-entered items from this activity
+		updateIndexFile();
+		
+		// Now upload everything
+		if (v == btnUpload) {
+			// If keys from previous session stored, use those; otherwise begin standard authentication
+			Context context = this;
+			String key = PrefsUtils.getPref(context, context.getString(R.string.dropboxUserToken_key));
+			String secret = PrefsUtils.getPref(context, context.getString(R.string.dropboxUserToken_secret));
+			AndroidAuthSession session = buildDropboxSession(key, secret);
+			
 			mDBApi = new DropboxAPI<AndroidAuthSession>(session);
 			// 3 possibilities:			
 			if (!mDBApi.getSession().isLinked()) {
@@ -153,9 +194,17 @@ public class IndexPageAttributesEditor extends Activity implements OnClickListen
 			zipShipTask = new ZipAndShip(localStoryDir.getName(), chkUploadWebTemplateFiles.isChecked(), this);
 			zipShipTask.execute();
 		}
-	}
+	}*/
 	
-	/** Reads in template index_template.html file, makes some configuration substitutions,
+	private void uploadFiles() {
+		Intent intent = new Intent();
+		intent.setClass(this, UploadMapStoryToDropboxSvc.class);
+		intent.putExtra("storyDir", localStoryDir.getAbsolutePath());
+		intent.putExtra("uploadTemplateFiles", chkUploadWebTemplateFiles.isChecked());
+		startService(intent);
+	}
+
+/** Reads in template index_template.html file, makes some configuration substitutions,
 	 *  and saves back out to an index.html file that will be uploaded or shipped.
 	 */
 	private void updateIndexFile() {
@@ -215,16 +264,17 @@ public class IndexPageAttributesEditor extends Activity implements OnClickListen
 		super.onResume();
 		if (currentlyAuthenticating) {
 			currentlyAuthenticating = false;
-			if (mDBApi.getSession() == null) // Dropbox authentication was unsuccessful
+			DropboxAPI<AndroidAuthSession> dbApi = ((MapStoryBuilderApplication) getApplication()).dbApi;
+			if (dbApi.getSession() == null) // Dropbox authentication was unsuccessful
 				UIUtils.showAlert(this, "This requires a free Dropbox account.");
 			// Dropbox authentication has been attempted - maybe successfully, maybe not
-			if (mDBApi.getSession().authenticationSuccessful()) { // Dropbox authentication was successful
+			if (dbApi.getSession().authenticationSuccessful()) { // Dropbox authentication was successful
 				try {
 					// MANDATORY call to complete auth.
 					// Sets the access token on the session
-					mDBApi.getSession().finishAuthentication();
+					dbApi.getSession().finishAuthentication();
 
-					AccessTokenPair tokens = mDBApi.getSession().getAccessTokenPair();
+					AccessTokenPair tokens = dbApi.getSession().getAccessTokenPair();
 
 					// Provide your own storeKeys to persist the access token pair
 					// A typical way to store tokens is using SharedPreferences
@@ -233,8 +283,9 @@ public class IndexPageAttributesEditor extends Activity implements OnClickListen
 
 					// Currently, the user only invokes authentication by clicking the "Share to Dropbox" button
 					// So we know to continue the upload once authentication succeeds
-					uploadTask = new UploadMapStoryToDropbox(localStoryDir.getName(), chkUploadWebTemplateFiles.isChecked(), IndexPageAttributesEditor.this);
-					uploadTask.execute(mDBApi);
+					uploadFiles();
+/*					uploadTask = new UploadMapStoryToDropbox(localStoryDir.getName(), chkUploadWebTemplateFiles.isChecked(), IndexPageAttributesEditor.this);
+					uploadTask.execute(dbApi);*/
 				} catch (IllegalStateException e) {
 					Log.i("DbAuthLog", "Error authenticating", e);
 					UIUtils.showAlert(this, "Encountered a problem authenticating with Dropbox:\n" + e.getMessage());
@@ -244,11 +295,36 @@ public class IndexPageAttributesEditor extends Activity implements OnClickListen
 	}
 
 	@Override
+	protected void onRestoreInstanceState(Bundle savedInstanceState) {
+		super.onRestoreInstanceState(savedInstanceState);
+		
+		txtMapTitle.setText(savedInstanceState.getString("mapTitle"));
+		txtIntroTitle.setText(savedInstanceState.getString("introTitle"));
+		txtMapSubtitle.setText(savedInstanceState.getString("mapSubtitle"));
+		txtIntroDesc.setText(savedInstanceState.getString("introDesc"));
+	}
+
+	@Override
+	protected void onSaveInstanceState(Bundle outState) {
+		super.onSaveInstanceState(outState);
+		
+		outState.putString("mapTitle", txtMapTitle.getText().toString());
+		outState.putString("introTitle", txtIntroTitle.getText().toString());
+		outState.putString("mapSubtitle", txtMapSubtitle.getText().toString());
+		outState.putString("introDesc", txtIntroDesc.getText().toString());
+	}
+
+/*	@Override
+	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+		// TODO Auto-generated method stub
+		super.onActivityResult(requestCode, resultCode, data);
+	}*/
+/*	@Override
 	public Object onRetainNonConfigurationInstance() {
 		// We know the activity is going away and we need to save the asynctasks
 		if (uploadTask != null) {
 			uploadTask.detach();
 		}
 		return uploadTask;
-	}
+	}*/
 }
